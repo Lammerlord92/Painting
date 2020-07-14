98e257bd3f2ed2990483aa16e267f035dd1f624e
JavCarFer <javcarfer92@gmail.com>
2020-07-14 11:31:05 +0200

    BATMAN: this commit has no parents

---
+++ src/main/java/org/javcarfer/App.java
@@ -0,0 +1,13 @@
+package org.javcarfer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
