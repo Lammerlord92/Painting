fcbaf4b50a95c672680e4e03629547d3a1838fb3
JavCarFer <javcarfer92@gmail.com>
2020-07-14 12:35:56 +0200

    Request mapping and pathVariable

--- src/main/java/org/javcarfer/controllers/GreetingController.java
+++ src/main/java/org/javcarfer/controllers/GreetingController.java
@@ -2,12 +2,20 @@ package org.javcarfer.controllers;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    @GetMapping(value="greetings/{x}/{y}", produces= MediaType.TEXT_PLAIN_VALUE)
+    public String greeting(@PathVariable("x") String x,
+                           @PathVariable("y") int y){
+        return "Hello again "+x+" "+y;
+    }
+    //greetings?x="aaaa"
     @GetMapping(value="greetings", produces= MediaType.TEXT_PLAIN_VALUE)
-    public String greeting(){
-        return "Hello again painter!";
+    public String greeting(@RequestParam("x") String x){
+        return "Hello again "+x;
     }
 }
