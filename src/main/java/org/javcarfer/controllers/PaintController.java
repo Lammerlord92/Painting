336e25dbe1f2054520c725ac99480b1304201d8f
JavCarFer <javcarfer92@gmail.com>
2020-07-14 14:38:55 +0200

    Testing delete, put & post

--- src/main/java/org/javcarfer/controllers/PaintController.java
+++ src/main/java/org/javcarfer/controllers/PaintController.java
@@ -2,9 +2,7 @@ package org.javcarfer.controllers;
 
 import org.javcarfer.domain.Paint;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -21,6 +19,7 @@ public class PaintController {
         paints.add(new Paint("Athonian camoshade","Citadel",""));
     }
 
+    //Get
     @GetMapping(value="paintTest", produces= MediaType.APPLICATION_JSON_VALUE)
     public Paint paintTest(){
         return new Paint("Gory red","Vallejo","");
@@ -35,6 +34,22 @@ public class PaintController {
                 .stream()
                 .filter(paint -> paint.getName().contains(filter) || paint.getBranch().contains(filter))
                 .collect(Collectors.toList());
+    }
+    //Create
+    @PostMapping(value="paint",consumes=MediaType.APPLICATION_JSON_VALUE)
+    public void createPaint(@RequestBody Paint paint){
+        paints.add(paint);
+    }
+    //Update by name (no Id at this moment)
+    @PutMapping(value="paint",consumes=MediaType.APPLICATION_JSON_VALUE)
+    public void updatePaint(@RequestBody Paint paint){
+        paints.remove(paints.stream().filter(p->paint.getName().equals(p)).findFirst().get());
+        paints.add(paint);
+    }
+    //Deleting by object (no Id at this moment)
+    @DeleteMapping(value="paint",consumes=MediaType.APPLICATION_JSON_VALUE)
+    public void deletePaint(@RequestBody Paint paint){
+        paints.remove(paint);
     }
 
 }
